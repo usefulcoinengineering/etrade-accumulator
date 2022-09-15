55860378e3e8fe04617cb8af5230a23a028d1d34
munair <munair@gmail.com>
2022-09-15 16:22:23 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='etrade-accumulator',
+    version='1.0',
+    packages=find_packages(),
+    long_description=open('README.md').read()
+)
+
